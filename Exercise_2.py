bac241ae35f019e27c2f111a1539338a6e4ee430
Aastha Dua <duaaastha3@gmail.com>
2024-05-03 13:13:06 -0700

    no commit

--- Exercise_2.py
+++ Exercise_2.py
@@ -13,6 +13,7 @@ def quickSort(arr,low,high):
     #write your code here
   
 # Driver code to test above 
+# this looks like in place sorting! 
 arr = [10, 7, 8, 9, 1, 5] 
 n = len(arr) 
 quickSort(arr,0,n-1) 
