aad648c48e30be11ab9c97a086d23da5b17bdbaf
Aastha Dua <duaaastha3@gmail.com>
2024-05-05 14:58:42 -0700

    quickSort Completed

--- Exercise_2.py
+++ Exercise_2.py
@@ -1,16 +1,26 @@
-# Python program for implementation of Quicksort Sort 
+# Python program for recursively implementation of Quicksort Sort 
   
 # give you explanation for the approach
-def partition(arr,low,high):
-  
-  
-    #write your code here
+def partition(array,low,high):
+    pivot = array[high]
+
+    i = low-1 
+
+    for j in range(low, high):
+        if array[j] <= pivot:
+            i+=1 
+            array[i] , array[j] = array[j] , array[i]
+    
+    array[i+1] , array[high] = array[high], array[i+1]
+    return i+1
   
 
 # Function to do Quick sort 
 def quickSort(arr,low,high): 
-    
-    #write your code here
+    if low<high:
+        pi = partition(arr, low, high)
+        quickSort(arr, low, pi-1)
+        quickSort(arr, pi+1, high)
   
 # Driver code to test above 
 # this looks like in place sorting! 
@@ -19,6 +29,4 @@ n = len(arr)
 quickSort(arr,0,n-1) 
 print ("Sorted array is:") 
 for i in range(n): 
-    print ("%d" %arr[i]), 
-  
- 
+    print ("%d" %arr[i])
--- Exercise_5.py
+++ Exercise_5.py
@@ -1,10 +1,37 @@
 # Python program for implementation of Quicksort
 
 # This function is same in both iterative and recursive
-def partition(arr, l, h):
-  #write your code here
+def partition(array, low, high):
+  pivot = array[high]
+  i = low-1
+
+  for j in range(low, high):
+    if array[j] <= pivot:
+      i+=1 
+      array[i], array[j] = array[j], array[i]
+  
+  array[i+1] , array[high] = array[high] , array[i+1]
+  return i+1
+
+def quickSortIterative(arr, low, high):
+  stack = []
+  stack.append((low, high))
+
+  while stack :
+    low, high = stack.pop()
+
+    if low < high:
+      pi = partition(arr, low, high)
+
+      stack.append((low, pi-1))
+      stack.append((pi+1, high))
 
 
-def quickSortIterative(arr, l, h):
-  #write your code here
-
+# Driver code to test above 
+# this looks like in place sorting! 
+arr = [10, 7, 8, 9, 1, 5] 
+n = len(arr) 
+quickSortIterative(arr,0,n-1) 
+print ("Sorted array is:") 
+for i in range(n): 
+    print ("%d" %arr[i]), 
